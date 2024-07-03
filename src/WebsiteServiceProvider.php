0e238975c4e703efcd55f70998d44846db663a78
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-07-04 00:28:28 +0200

    wip

---
+++ src/WebsiteServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nidavellir\Website;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackendServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
